5fb6ac8ab78419b1ba264d0a94798b2db09ae485
Joao Ciardullo <joaociardullo@hotmail.com>
2021-09-25 16:50:27 -0300

    Inicio de Projeto

---
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -0,0 +1,10 @@
+package br.com.springboot.curso_jdev_treinamento.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity   //sempre quando usar com o banco de dados 
+public class  Usuario implements Serializable {
+
+}
