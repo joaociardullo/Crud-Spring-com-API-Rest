58d0b49816274a020e443aec421a078b9c35d58d
Joao Ciardullo <joaociardullo@hotmail.com>
2021-09-26 16:18:19 -0300

    Salvar Todos com PostMapping pela requisição do corpo

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,44 +23,51 @@ import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
  */
 @RestController
 public class GreetingsController {
-	
-	@Autowired /*CD/IC/CDI - inhenção de depedencia */
-	
+
+	@Autowired /* CD/IC/CDI - inhenção de depedencia */
+
 	private UsuarioRepository usuarioRepository;
-	
-	
-	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/meu/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
+
+	/**
+	 *
+	 * @param name the name to greet
+	 * @return greeting text
+	 */
+	@RequestMapping(value = "/meu/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String Teste(@PathVariable String nome) {
+
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+
+		usuarioRepository.save(usuario); /* grava no banco de dados */
+
+		return "Joao na voz " + nome;
+	}
+
+	@GetMapping(value = "listatodos") /* Primeiro Metodo de API, Buscando do banco de dados */
+	@ResponseBody /* Retorna os dados para o corpo da resposta */
+	public ResponseEntity<java.util.List<Usuario>> listarUsuario() {
+
+		java.util.List<Usuario> usuarios = usuarioRepository.findAll(); // FIndALl Retorna tudo do banco de dados
+																		// /*Executa a consulta no banco de dados*/
+
+		return new ResponseEntity<java.util.List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista em JSON
+	}
+
+	@PostMapping(value = "salvar")/*Mapeia a URL   Enviar os dados dentro da requisição via post*/ 
+    @ResponseBody/*Descrição da resposta*/
+    public ResponseEntity<Usuario>salvar(@RequestBody Usuario usuario){
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+   	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
-    
-    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String Teste(@PathVariable String nome) {
-    	
-    	Usuario usuario = new Usuario();
-    	usuario.setNome(nome);
-    	
-    	usuarioRepository.save(usuario); /*grava no banco de dados*/
-    	
-        return "Joao na voz " + nome;
-    }
-    
-    
-    @GetMapping(value = "listatodos") /*Primeiro Metodo de API, Buscando do banco de dados*/
-    @ResponseBody /* Retorna os dados para o corpo da resposta*/
-    public ResponseEntity<java.util.List<Usuario>> listarUsuario(){
-    	
-    	java.util.List<Usuario> usuarios = usuarioRepository.findAll();   //FIndALl Retorna tudo do banco de dados  /*Executa a consulta no banco de dados*/   
-    	
-    	return new ResponseEntity<java.util.List<Usuario>>(usuarios, HttpStatus.OK); //Retorna  a lista em JSON
-    }
-    
+
 }
