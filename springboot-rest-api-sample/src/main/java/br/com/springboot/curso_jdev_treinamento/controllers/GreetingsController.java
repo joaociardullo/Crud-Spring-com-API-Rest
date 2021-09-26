c182772fed36f2fa626de2045278bb309e3cc751
Joao Ciardullo <joaociardullo@hotmail.com>
2021-09-26 15:53:42 -0300

    Buscando os dados no banco de dados e retornando em json

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -2,12 +2,16 @@ package br.com.springboot.curso_jdev_treinamento.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import antlr.collections.List;
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
@@ -46,4 +50,15 @@ public class GreetingsController {
     	
         return "Joao na voz " + nome;
     }
+    
+    
+    @GetMapping(value = "listatodos") /*Primeiro Metodo de API, Buscando do banco de dados*/
+    @ResponseBody /* Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<java.util.List<Usuario>> listarUsuario(){
+    	
+    	java.util.List<Usuario> usuarios = usuarioRepository.findAll();   //FIndALl Retorna tudo do banco de dados  /*Executa a consulta no banco de dados*/   
+    	
+    	return new ResponseEntity<java.util.List<Usuario>>(usuarios, HttpStatus.OK); //Retorna  a lista em JSON
+    }
+    
 }
